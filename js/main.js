1d96e083073b75d21811fe07c2c145bebfc5b1f5
bacorero <bacorero65@gmail.com>
2021-08-09 20:47:25 +0200

    Pongo geolocalizacion

---
+++ js/main.js
@@ -0,0 +1,9 @@
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mostrarUbicacion);
+  }
+  
+  function mostrarUbicacion (ubicacion) {
+    const lng = ubicacion.coords.longitude;
+    const lat = ubicacion.coords.latitude;
+    console.log(`longitud: ${ lng } | latitud: ${ lat }`);
+  }
