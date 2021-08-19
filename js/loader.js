5864fcae70242f60b2f2c303fad45be30b508fda
bacorero <bacorero65@gmail.com>
2021-08-19 18:19:56 +0200

    Objetos por array

---
+++ js/loader.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+
+    let places = staticLoadPlaces();            //Cargamos los objetos
+
+    renderPlaces(places);                       //Los renderizamos
+};
+
+//Esta funcion devuelve los objetos predeterminados
+function staticLoadPlaces(){
+    return[
+        {
+            name:'a-box',
+            color:'yelow',
+            latitude:'38.346771',
+            longitude:'-0.506523'
+        },
+    ];
+}
+
+//Funcion que renderiza los objetos precargados
+function renderPlaces(places){
+    let scene = document.querySelector('a-scene');          //Apuntamos al tag 'a-scene'
+
+    //Recorremos los objetos para renderizarlos
+    places.foreach((place)=>{
+        let model = document.createElement(place.name);     //Creamos el objeto
+        let color = place.color;                            //Color del objeto
+        model.setAttribute('color',color);
+
+        //model.setAttribute('gps-entity-place');
+    })
+}
