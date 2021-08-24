ccc009fe317ed21f6daddd6ffff80baf1c55268e
bacorero <bacorero65@gmail.com>
2021-08-24 19:09:04 +0200

    Vuelta atras

--- js/loader.js
+++ js/loader.js
@@ -1,38 +1,8 @@
-window.onload = () => {
-
-    let places = staticLoadPlaces();            //Cargamos los objetos
-
-    renderPlaces(places);                       //Los renderizamos
-};
-
-//Esta funcion devuelve los objetos predeterminados
-function staticLoadPlaces(){
-    return[
-        {
-            name:'a-box',
-            color:'blue',
-            latitude:'38.346771',
-            longitude:'-0.506523'
-        },
-        {
-            name:'a-box',
-            color:'red',
-            latitude:'38.346000',
-            longitude:'-0.506265'
-        },
-        {
-            name:'a-box',
-            color:'green',
-            latitude:'38.346464',
-            longitude:'-0.506308'
-        },
-    ];
-}
-
-//Funcion que renderiza los objetos precargados
-function renderPlaces(places){
-    let scene = document.querySelector('a-scene'); 
-    const box = document.createElement('a-box');
-    box.setAttribute('material','color:blue');
-    scene.appendChild(box);
-}
+AFRAME.registerComponent('object', {
+    init: function() {
+        this.loaded = false;
+        const entity = document.createElement('a-box');
+        entity.setAttribute('material', 'color:blue');
+        this.el.appendChild(entity);
+        }
+    });
