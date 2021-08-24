e3ea8634de5884cb728fad0cd8a435684a91ab0d
bacorero <bacorero65@gmail.com>
2021-08-24 19:55:26 +0200

    Vuelta atras

--- js/loader.js
+++ js/loader.js
@@ -2,8 +2,10 @@ window.onload = ()=> {
     let scene = document.querySelector('a-scene');
     const entity = document.createElement('entity');
     entity.setAttribute('gps-projected-entity-place','latitude: 38.346771; longitude: -0.506523');
-    entity.setAttribute('gltf-model','https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf')
+    entity.setAttribute('gltf-model','./assets/articuno/scene.gltf');
     entity.setAttribute('scale','0.1 0.1 0.1');
     
     scene.appendChild(entity);
 }
+
+
