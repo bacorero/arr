b8c8e294387c351ea3d51a75ceedb552f97a25e1
bacorero <bacorero65@gmail.com>
2021-08-24 19:19:05 +0200

    Vuelta atras

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,1 @@
-AFRAME.registerComponent('object', {
-    init: function() {
-        this.loaded = false;
-        const entity = document.createElement('a-box');
-        entity.setAttribute('material', 'color:blue');
-        this.el.appendChild(entity);
-        }
-    });
+
