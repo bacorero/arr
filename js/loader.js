c07f8ad78c24d43396fc82cda5b81d58be7585ce
bacorero <bacorero65@gmail.com>
2021-08-24 18:15:02 +0200

    Objetos por array

--- js/loader.js
+++ js/loader.js
@@ -33,6 +33,6 @@ function staticLoadPlaces(){
 function renderPlaces(places){
     let scene = document.querySelector('a-scene'); 
     const box = document.createElement('a-box');
-    box.setAttribute('material','color:blue');
+    //box.setAttribute('material','color:blue');
     scene.appendChild(box);
 }
