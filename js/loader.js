045cbbaf7c99f885fc524a94c3ae9a9d43859cf9
bacorero <bacorero65@gmail.com>
2021-08-25 16:23:27 +0200

    Comenzando de nuevo

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,7 @@
 window.onload = ()=> {
     let scene = document.querySelector('a-scene');
     const entity = document.createElement('entity');
-    entity.setAttribute('gps-projected-entity-place','latitude: 38.346771; longitude: -0.506523');
+    entity.setAttribute('gps-projected-entity-place','latitude: 38.346464; longitude: -0.506308');
     entity.setAttribute('gltf-model','./assets/scene.gltf');
     entity.setAttribute('scale','0.1 0.1 0.1');
     
