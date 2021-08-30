ce9352c97e277c20bc07d79e0f1ca2dfb98d8433
bacorero <bacorero65@gmail.com>
2021-08-30 18:19:40 +0200

    Comienzo basico con atributos solucionados

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,17 @@
-window.onload = ()=> {
-    let scene = document.querySelector('a-scene');
-    const entity = document.createElement('entity');
-    entity.setAttribute('gps-projected-entity-place','latitude: 38.346464; longitude: -0.506308');
-    entity.setAttribute('gltf-model','url(assets/scene.gltf)');
-    entity.setAttribute('scale','3 3 3');
-    
-    scene.appendChild(entity);
-}
 
+AFRAME.registerComponent('sceneel',{
+        init:function(){
+               /* var scena = document.querySelector('a-scene');
+                //scena.querySelector('a-sphere').setAttribute('position',{x:1,y:0,z:-6});
+                var esfera = scena.querySelector('a-sphere');
+                esfera.setAttribute('position',{x:1,y:1,z:-6});   
+                var caja = document.createElement('a-box');
+                caja.setAttribute('position',{x:2,y:2,z:-6});
+                caja.setAttribute('material','color','green');
+                caja.setAttribute('depth','5');
+                caja.setAttribute('height','5');
+                caja.setAttribute('width','5');
+                scena.appendChild(caja);*/
 
+        }
+});
