d7f780327aa85005fdc2193745c3252eb86abb9c
bacorero <bacorero65@gmail.com>
2021-08-31 18:27:38 +0200

    Comienzo basico con atributos solucionados

--- js/loader.js
+++ js/loader.js
@@ -4,6 +4,7 @@ AFRAME.registerComponent('sceneel',{
                 var scena = document.querySelector('a-scene');
                 var esfera = document.createElement('a-sphere');
                 var cilindro = document.createElement('a-cylinder');
+                var texto = document.createElement('a-text');
 
                 esfera.setAttribute('gps-projected-entity-place',{latitude:38.346401,longitude:-0.506278});
                 esfera.setAttribute('material','color','green');
@@ -14,11 +15,16 @@ AFRAME.registerComponent('sceneel',{
                 cilindro.setAttribute('geometry','height','3');
                 cilindro.setAttribute('geometry','radius','3');
 
+                texto.setAttribute('value','Hola,JM!!');
+                texto.setAttribute('text','color','green');
+                texto.setAttribute('gps-projected-entity-place',{latitude:38.345556,longitude:-0.505388});
+
                 //esfera.setAttribute('depth','5');
                 //esfera.setAttribute('height','5');
                 //esfera.setAttribute('width','5');
 
                 scena.appendChild(esfera);
                 scena.appendChild(cilindro);
+                scena.appendChild(texto);
         }
 });
