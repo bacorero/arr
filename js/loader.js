3504bdf423b2c178a768cc3b8d41cba35e2fb7a0
bacorero <bacorero65@gmail.com>
2021-09-07 20:03:21 +0200

    Cota de distancias

--- js/loader.js
+++ js/loader.js
@@ -2,12 +2,17 @@
 AFRAME.registerComponent('sceneel',{
         init:function(){
                 var scena = document.querySelector('a-scene');
+                var camara = document.querySelector('a-camera');
+                
                 var esfera = document.createElement('a-sphere');
                 var cilindro = document.createElement('a-cylinder');
                 var texto = document.createElement('a-text');
 
+                camara.setAttribute('maxDistance','10');
+
                 esfera.setAttribute('gps-projected-entity-place',{latitude:38.346401,longitude:-0.506278});
                 esfera.setAttribute('material','color','green');
+                esfera.setAttribute('look-at','[gps-projected-camera]');
 
                 cilindro.setAttribute('gps-projected-entity-place',{latitude:38.346390,longitude:-0.506200});
                 cilindro.setAttribute('material','color','blue');
@@ -15,9 +20,9 @@ AFRAME.registerComponent('sceneel',{
                 cilindro.setAttribute('geometry','height','3');
                 cilindro.setAttribute('geometry','radius','3');
 
-                texto.setAttribute('value','Hola,JM!!');
+                //texto.setAttribute('value','Hola,JM!!');
                 //texto.setAttribute('text','color','green');
-                texto.setAttribute('gps-projected-entity-place',{latitude:38.345556,longitude:-0.505388});
+                //texto.setAttribute('gps-projected-entity-place',{latitude:38.345556,longitude:-0.505388});
 
                 //esfera.setAttribute('depth','5');
                 //esfera.setAttribute('height','5');
@@ -25,6 +30,6 @@ AFRAME.registerComponent('sceneel',{
 
                 scena.appendChild(esfera);
                 scena.appendChild(cilindro);
-                scena.appendChild(texto);
+                //scena.appendChild(texto);
         }
 });
